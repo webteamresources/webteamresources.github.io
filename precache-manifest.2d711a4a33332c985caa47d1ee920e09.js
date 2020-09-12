1b49343900d5cdb6aa2e5bd17c079b9b228669f4
webteamresources <58063279+webteamresources@users.noreply.github.com>
2020-09-12 19:09:32 +0530

    Add files via upload

---
+++ precache-manifest.2d711a4a33332c985caa47d1ee920e09.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f72c746d00ec9a5e31f430f66a0dcf70",
+    "url": "/index.html"
+  },
+  {
+    "revision": "78b70cd7897871627eb1",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "4d8fb9eb30fe4c3b1050",
+    "url": "/static/js/2.ad89822f.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.ad89822f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78b70cd7897871627eb1",
+    "url": "/static/js/main.3744d033.chunk.js"
+  },
+  {
+    "revision": "b2d4be5e228c1b2af425",
+    "url": "/static/js/runtime-main.1841fe77.js"
+  }
+]);
